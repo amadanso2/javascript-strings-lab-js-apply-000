98b196fd14e533fc69f4367c13dc1818e8c0ebf8
Ama Danso <ama5danso@yahoo.com>
2018-10-12 20:13:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "Hi, Neil deGrasse Tyson" + specialGuest + "hi!";
+var greetSpecialGuest = "Hello, Neil deGrasse Tyson" + specialGuest + "Hello, Neil deGrasse Tyson!";
 
 var topic = "space";
 
